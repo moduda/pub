52e0962bddeffdbd6b471fbc4db53db7b7cd1179
Yoon Kim <yoon@tentmakers.com>
2020-02-17 18:25:23 -0800

    Values moved to varialbes at the top

--- tools/macos/set-screensaver.sh
+++ tools/macos/set-screensaver.sh
@@ -5,12 +5,16 @@
 
 # Deposit photo file in here
 FOLDERPATH="/Users/Shared/screensaver"
+# Idle time in seconds
+IDLETIME=7200
+# Display clock true/false
+SHOWCLOCK=true
 
 ## Set Screensaver to Photo Slideshow
 /usr/bin/defaults -currentHost write com.apple.screensaver 'CleanExit' -string "YES"
 /usr/bin/defaults -currentHost write com.apple.screensaver "moduleDict" -dict-add "path" -string "/System/Library/Frameworks/ScreenSaver.framework/Resources/iLifeSlideshows.saver"
 /usr/bin/defaults -currentHost write com.apple.screensaver "moduleDict" -dict-add "type" -int "0" 
-/usr/bin/defaults -currentHost write com.apple.screensaver 'ShowClock' -bool "false"
+/usr/bin/defaults -currentHost write com.apple.screensaver 'ShowClock' -bool "$SHOWCLOCK"
 /usr/bin/defaults -currentHost write com.apple.screensaver 'tokenRemovalAction' -int "0"
 
 ## Set Type of Slideshow to "Flipup" (Results inconsistent)
@@ -20,6 +24,6 @@ FOLDERPATH="/Users/Shared/screensaver"
 /usr/bin/defaults -currentHost write com.apple.ScreenSaverPhotoChooser 'SelectedSource' -int "4"
 /usr/bin/defaults -currentHost write com.apple.ScreenSaverPhotoChooser 'SelectedFolderPath' "$FOLDERPATH"
 /usr/bin/defaults -currentHost write com.apple.ScreenSaverPhotoChooser 'ShufflesPhotos' -bool "true"
-/usr/bin/defaults -currentHost write com.apple.screensaver idleTime 7200
+/usr/bin/defaults -currentHost write com.apple.screensaver idleTime $IDLETIME
 
 killall cfprefsd
