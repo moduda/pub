f6f367ecc193b06320c94e65cf44dcf7fb0e2cdb
Yoon Kim <yoon@tentmakers.com>
2026-05-12 11:20:15 -0700

    Updated openssl options

--- tools/macos/encryptfile.sh
+++ tools/macos/encryptfile.sh
@@ -11,10 +11,10 @@ FILE="$1"
 
 case $(file $FILE) in
 *"openssl enc'd data with salted password"*)
-	openssl enc -d -aes-256-cbc -in $FILE -out ${FILE}.decrypt
+	openssl enc -d -aes-256-cbc -pbkdf2 -iter 100000 -in $FILE -out ${FILE}.decrypt
 	;;
 *)
-	openssl enc -aes-256-cbc -salt -in $FILE -out ${FILE}.enc
+    openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -salt -in $FILE -out ${FILE}.enc
 	;;
 esac
 
