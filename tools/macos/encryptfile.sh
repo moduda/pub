b9b65c9057ccea9cb9d775247da715acc47d1835
Yoon Kim <yoon@tentmakers.com>
2026-05-12 11:24:50 -0700

    chmod 600 output files

--- tools/macos/encryptfile.sh
+++ tools/macos/encryptfile.sh
@@ -12,9 +12,11 @@ FILE="$1"
 case $(file $FILE) in
 *"openssl enc'd data with salted password"*)
 	openssl enc -d -aes-256-cbc -pbkdf2 -iter 100000 -in $FILE -out ${FILE}.decrypt
+    chmod 600 ${FILE}.decrypt
 	;;
 *)
     openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -salt -in $FILE -out ${FILE}.enc
+    chmod 600 ${FILE}.enc
 	;;
 esac
 
